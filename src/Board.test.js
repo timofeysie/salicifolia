6fe25e4fb86399b863c45c03e451012bd57e7c79
Timothy Curchod <timofeyc@hotmail.com>
2018-06-09 08:23:11 +1000

    Finisehd tests for board

--- src/Board.test.js
+++ src/Board.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Board from './board';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
@@ -8,4 +8,12 @@ configure({ adapter: new Adapter() });
 it('renders without crashing', () => {
     let squares = Array(9).fill(null)
     shallow(<Board squares={squares}/>);
+});
+
+it('calls onClick event on click of a board square', () =>{
+    let squares = Array(9).fill(null)
+    const onClick = jest.fn();
+    let wrapper = mount(<Board squares={squares} onClick={onClick}/>);
+    wrapper.find('button.square').first().simulate('click');
+    expect(onClick).toBeCalledWith(0)
 });
--- src/Game.test.js
+++ src/Game.test.js
@@ -4,19 +4,47 @@ import Board from './Board.js';
 import Square from './Square.js';
 import renderer from 'react-test-renderer';
 import Enzyme from 'enzyme';
-import {shallow} from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
-test('tree to Match Snapshot', () => {
-  const component = renderer.create(
-    <Game />,
-  );
+it('tree to Match Snapshot', () => {
+  const component = renderer.create( <Game />, );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
-test('Game board is created', () => {
-  const game = shallow(<Game />);
+// test('Game board is created', () => {
+//   const game = shallow(<Game />);
+// });
+
+it('renders without crashing', () => {
+  shallow(<Game />);
+});
+
+it('renders game status correctly', () => {
+  const wrapper = mount(<Game/>);
+  const firstPlayer = wrapper.find('div.game-info').children().first().text();
+  expect(firstPlayer).toEqual('Next player: X');
+  const button = wrapper.find('button.square').first();
+  button.simulate('click');
+  const secondPlayer = wrapper.find('div.game-info').children().first().text();
+  expect(secondPlayer).toEqual('Next player: O');
+
+  //player 2
+  const turn2 = wrapper.find('button.square').at(1);
+  turn2.simulate('click');
+  //player 1
+  const turn3 = wrapper.find('button.square').at(4);
+  turn3.simulate('click');
+  //player 2
+  const turn4 = wrapper.find('button.square').at(5);
+  turn4.simulate('click');
+  //player 1
+  const turn5 = wrapper.find('button.square').at(8);
+  turn5.simulate('click');
+  
+  const winner = wrapper.find('div.game-info').children().first().text();
+  expect(winner).toEqual('Winner: X');
 });
